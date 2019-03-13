26b9b312fb0c76be4143b884258d0425c1153bf1
Raghava Lakshminarayana <raglaks94@gmail.com>
2019-03-13 12:45:59 -0600

    Updates

---
+++ precache-manifest.b24e672bccce23c57dbbb4609aea629c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70e38b4efb54f6272b41",
+    "url": "/madkting-test/static/css/main.9e5c4089.chunk.css"
+  },
+  {
+    "revision": "70e38b4efb54f6272b41",
+    "url": "/madkting-test/static/js/main.50ad9f02.chunk.js"
+  },
+  {
+    "revision": "a69cd61a569553235217",
+    "url": "/madkting-test/static/js/runtime~main.d458f758.js"
+  },
+  {
+    "revision": "38eeff9741a8794d4147",
+    "url": "/madkting-test/static/js/2.fed86b58.chunk.js"
+  },
+  {
+    "revision": "32288f9fd877925ed7628606b10edaf0",
+    "url": "/madkting-test/index.html"
+  }
+];
